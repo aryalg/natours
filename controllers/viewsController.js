8c059f8eaa2289621526832e8fa3e036690706e3
Jonas Schmedtmann <email>
2019-11-26 16:54:17 +0545

    Insterted next() function to alert

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -11,6 +11,7 @@ exports.alerts = (req, res, next) => {
       'Your booking was successful! Please check your email for a confirmation. If your booking doesnot show up here immediately, please come back later';
     next();
   }
+  next();
 };
 
 exports.getOverview = catchAsync(async (req, res, next) => {
